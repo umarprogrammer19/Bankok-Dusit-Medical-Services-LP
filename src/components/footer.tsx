8cf35651370ba0f353784fc7dd31db4c8e0e591c
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-12-19 17:38:07 +0500

    Footer Done Responsive

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -6,12 +6,12 @@ export function FooterSection() {
         <Wrapper className="bg-[url('/footer-background.png')]">
             <footer className="relative overflow-hidden">
                 <div className="relative z-10 py-8 md:py-16">
-                    <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="container mx-auto sm:px-6 lg:px-8">
                         <div className="flex flex-col md:flex-row justify-between items-start gap-6">
                             {/* Left Content */}
                             <div className="space-y-12">
                                 {/* Contact Info */}
-                                <div className="flex flex-wrap items-center gap-4 md:gap-6 text-white text-xs md:text-lg">
+                                <div className="flex flex-wrap items-center gap-4 md:gap-6 text-white text-sm md:text-lg">
                                     <a href="tel:+966533973111" className="flex items-center gap-2 hover:text-[#c5e4f5] transition-colors">
                                         <Image src="/footer/phone.svg" alt="Phone" width={16} height={16} className="w-5 h-5" />
                                         +966533973111
@@ -34,15 +34,21 @@ export function FooterSection() {
                                     </a>
                                 </div>
 
+                                {/* Right Content - Address */}
+                                <div className="text-white items-end text-sm md:text-[17px] text-left md:text-left md:hidden block">
+                                    <p>3293 Abdullah Ibn Hunthafa Al Sahmi,</p>
+                                    <p>Diplomatic Quarter, Riyadh, Saudi Arabia</p>
+                                </div>
+
                                 {/* Company Info */}
-                                <div className="text-white/80 text-xs md:text-[16px] space-y-1">
+                                <div className="text-white/80 text-sm md:text-[16px] space-y-1">
                                     <p>Bangkok Dusit Medical Services PCL (BDMS) Thailand's leading private hospital network</p>
                                     <p>© 2026 BDMS. All rights reserved</p>
                                 </div>
                             </div>
 
                             {/* Right Content - Address */}
-                            <div className="text-white items-end text-xs md:text-[17px] text-left md:text-left">
+                            <div className="text-white items-end text-xs md:text-[17px] text-left md:text-left md:block hidden">
                                 <p>3293 Abdullah Ibn</p>
                                 <p>Hunthafa Al Sahmi,</p>
                                 <p>Diplomatic Quarter,</p>
